117471b372b990511b86614873637a0ce3108bd6
uerbzr <nigel.sibbert@gmail.com>
2023-04-19 08:30:42 +0100

    Add project files.

---
+++ csharp-strings-workshop.Main/StringSamples.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_strings_workshop.Main
+{
+    /// <summary>
+    /// Class to describe the different ways of joing two strings together!
+    /// </summary>
+    public class StringSamples
+    {
+        public string StringInterpolationExample()
+        {
+            string firstName = "Albert";
+            string lastName = "Einstein";
+            string fullName = $"{firstName} {lastName}";
+            return fullName;
+        }
+        public string StringBuilderExample()
+        {
+            string firstName = "Albert";
+            string lastName = "Einstein";
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(firstName);
+            stringBuilder.Append(" ");
+            stringBuilder.Append(lastName);
+            string fullName = stringBuilder.ToString();
+            return fullName;
+        }
+        public string JoinExample()
+        {
+            string[] names = { "Albert", "Einstein" };
+            string fullName = string.Join(" ", names);
+            return fullName;
+        }
+        public string ConcatExample()
+        {
+            string firstName = "Albert";
+            string lastName = "Einstein";
+            string fullName = string.Concat(firstName, " ", lastName);
+            return fullName;
+        }
+        public string PlusOperatorExample()
+        {
+            string firstName = "Albert";
+            string lastName = "Einstein";
+            string fullName = firstName + " " + lastName;
+            return fullName;
+        }
+        public string FormatExample()
+        {
+            string firstName = "Albert";
+            string lastName = "Einstein";
+            string fullName = string.Format("{0} {1}", firstName, lastName);
+            return fullName;
+        }
+                             
+    }
+}
---
+++ csharp-strings-workshop.Tests/Tests.cs
@@ -0,0 +1,27 @@
+﻿using csharp_strings_workshop.Main;
+using NUnit.Framework;
+
+namespace csharp_strings_workshop.Tests
+{
+    
+    public class Tests
+    {
+        StringSamples _stringSamples;
+        private readonly string _name = "Albert Einstein"; 
+        public Tests()
+        {
+            _stringSamples = new StringSamples();
+            
+        }
+        [TestCase]
+        public void TestStrings()
+        {
+            Assert.IsTrue(_name == _stringSamples.StringInterpolationExample());
+            Assert.IsTrue(_name == _stringSamples.StringBuilderExample());
+            Assert.IsTrue(_name == _stringSamples.JoinExample());
+            Assert.IsTrue(_name == _stringSamples.ConcatExample());
+            Assert.IsTrue(_name == _stringSamples.PlusOperatorExample());
+            Assert.IsTrue(_name == _stringSamples.FormatExample());
+        }
+    }
+}
